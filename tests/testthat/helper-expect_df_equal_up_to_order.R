e593aa508ae6f6d4e440ed2491c05d9c3f86a261
Evan Ray <elray1@users.noreply.github.com>
2025-01-03 09:41:52 -0500

    Update tests/testthat/helper-expect_df_equal_up_to_order.R
    Co-authored-by: Zhian N. Kamvar <zkamvar@gmail.com>

--- tests/testthat/helper-expect_df_equal_up_to_order.R
+++ tests/testthat/helper-expect_df_equal_up_to_order.R
@@ -2,7 +2,7 @@
 #'
 #' @param df_act The actual data frame
 #' @param df_exp The expected data frame
-expect_df_equal_up_to_order <- function(df_act, df_exp, ignore_attr = FALSE) {
+expect_df_equal_up_to_order <- function(df_act, df_exp, ignore_attr = FALSE, ...) {
   cols <- colnames(df_act)
   testthat::expect_equal(cols, colnames(df_exp))
   testthat::expect_equal(
