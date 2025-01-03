13959af80a65a2453f018808b9f4e5d611e351dd
Evan Ray <elray1@users.noreply.github.com>
2025-01-03 09:42:01 -0500

    Update tests/testthat/helper-expect_df_equal_up_to_order.R
    Co-authored-by: Zhian N. Kamvar <zkamvar@gmail.com>

--- tests/testthat/helper-expect_df_equal_up_to_order.R
+++ tests/testthat/helper-expect_df_equal_up_to_order.R
@@ -8,6 +8,7 @@ expect_df_equal_up_to_order <- function(df_act, df_exp, ignore_attr = FALSE, ...
   testthat::expect_equal(
     dplyr::arrange(df_act, dplyr::across(dplyr::all_of(cols))),
     dplyr::arrange(df_exp, dplyr::across(dplyr::all_of(cols))),
-    ignore_attr = ignore_attr
+    ignore_attr = ignore_attr,
+    ...
   )
 }
