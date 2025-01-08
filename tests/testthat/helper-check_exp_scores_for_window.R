6495df9171a35abe7c1afe6fb96b311be79c1181
Evan L. Ray <elray@umass.edu>
2025-01-08 16:54:08 -0500

    refactor expected scores to fixtures

--- tests/testthat/helper-check_exp_scores_for_window.R
+++ tests/testthat/helper-check_exp_scores_for_window.R
@@ -8,78 +8,27 @@
 check_exp_scores_for_window <- function(out_path, window_name, model_out_tbl, oracle_output,
                                         include_rel = FALSE) {
   # check that the output files were created and have the expected contents
-  # no disaggregation
-  scores_path <- file.path(out_path, "wk inc flu hosp", window_name, "scores.csv")
-  testthat::expect_true(file.exists(scores_path))
-
-  actual_scores <- read.csv(scores_path)
-  expected_mean_scores <- hubEvals::score_model_out(
-    model_out_tbl = model_out_tbl |> dplyr::filter(.data[["output_type"]] == "mean"),
-    oracle_output = oracle_output,
-    metrics = "se_point",
-    relative_metrics = if (include_rel) "se_point" else NULL,
-    baseline = "FS-base",
-    by = "model_id"
-  )
-  expected_median_scores <- hubEvals::score_model_out(
-    model_out_tbl = model_out_tbl |> dplyr::filter(.data[["output_type"]] == "median"),
-    oracle_output = oracle_output,
-    metrics = "ae_point",
-    relative_metrics = if (include_rel) "ae_point" else NULL,
-    baseline = "FS-base",
-    by = "model_id"
-  )
-  expected_quantile_scores <- hubEvals::score_model_out(
-    model_out_tbl = model_out_tbl |> dplyr::filter(.data[["output_type"]] == "quantile"),
-    oracle_output = oracle_output,
-    metrics = c("wis", "ae_median", "interval_coverage_50", "interval_coverage_95"),
-    relative_metrics = if (include_rel) c("wis", "ae_median") else NULL,
-    baseline = "FS-base",
-    by = "model_id"
-  )
-  expected_scores <- expected_mean_scores |>
-    dplyr::left_join(expected_median_scores, by = "model_id") |>
-    dplyr::left_join(expected_quantile_scores, by = "model_id")
-  expect_df_equal_up_to_order(actual_scores, expected_scores, ignore_attr = TRUE) # nolint: object_usage_linter
-
-  for (by in c("location", "reference_date", "horizon", "target_end_date")) {
-    # check that the output files were created and have the expected contents
-    # disaggregated by `by`
-    scores_path <- file.path(out_path, "wk inc flu hosp", window_name, by, "scores.csv")
+  # disaggregated by `by` if non-NULL, otherwise no disaggregation
+  for (by in list(NULL, "location", "reference_date", "horizon", "target_end_date")) {
+    if (is.null(by)) {
+      scores_path <- file.path(out_path, "wk inc flu hosp", window_name, "scores.csv")
+    } else {
+      scores_path <- file.path(out_path, "wk inc flu hosp", window_name, by, "scores.csv")
+    }
     testthat::expect_true(file.exists(scores_path))
 
     actual_scores <- read.csv(scores_path)
-    if (by %in% c("reference_date", "target_end_date")) {
-      actual_scores[[by]] <- as.Date(actual_scores[[by]])
+
+    expected_scores_path <- testthat::test_path(
+      "testdata", "expected-scores",
+      paste0("scores_", window_name, ifelse(is.null(by), "", paste0("_by_", by)), ".csv")
+    )
+    expected_scores <- read.csv(expected_scores_path)
+    if (!include_rel) {
+      expected_scores <- expected_scores |>
+        dplyr::select(-dplyr::contains("relative"))
     }
 
-    expected_mean_scores <- hubEvals::score_model_out(
-      model_out_tbl = model_out_tbl |> dplyr::filter(.data[["output_type"]] == "mean"),
-      oracle_output = oracle_output,
-      metrics = "se_point",
-      relative_metrics = if (include_rel) "se_point" else NULL,
-      baseline = "FS-base",
-      by = c("model_id", by)
-    )
-    expected_median_scores <- hubEvals::score_model_out(
-      model_out_tbl = model_out_tbl |> dplyr::filter(.data[["output_type"]] == "median"),
-      oracle_output = oracle_output,
-      metrics = "ae_point",
-      relative_metrics = if (include_rel) "ae_point" else NULL,
-      baseline = "FS-base",
-      by = c("model_id", by)
-    )
-    expected_quantile_scores <- hubEvals::score_model_out(
-      model_out_tbl = model_out_tbl |> dplyr::filter(.data[["output_type"]] == "quantile"),
-      oracle_output = oracle_output,
-      metrics = c("wis", "ae_median", "interval_coverage_50", "interval_coverage_95"),
-      relative_metrics = if (include_rel) c("wis", "ae_median") else NULL,
-      baseline = "FS-base",
-      by = c("model_id", by)
-    )
-    expected_scores <- expected_mean_scores |>
-      dplyr::left_join(expected_median_scores, by = c("model_id", by)) |>
-      dplyr::left_join(expected_quantile_scores, by = c("model_id", by))
     expect_df_equal_up_to_order(actual_scores, expected_scores, ignore_attr = TRUE) # nolint: object_usage_linter
   }
 }
---
+++ tests/testthat/testdata/create_exp_score_fixtures.R
@@ -0,0 +1,55 @@
+library(testthat)
+library(hubData)
+library(hubEvals)
+library(dplyr)
+
+hub_path <- testthat::test_path("testdata", "ecfh")
+model_out_tbl <- hubData::connect_hub(hub_path) |>
+  dplyr::collect()
+oracle_output <- read.csv(
+  test_path("testdata", "ecfh", "target-data", "oracle-output.csv")
+)
+oracle_output[["target_end_date"]] <- as.Date(oracle_output[["target_end_date"]])
+
+make_score_fixtures_one_window <- function(window_name, model_out_tbl) {
+  for (by in list(NULL, "location", "reference_date", "horizon", "target_end_date")) {
+    expected_mean_scores <- hubEvals::score_model_out(
+      model_out_tbl = model_out_tbl |> dplyr::filter(.data[["output_type"]] == "mean"),
+      oracle_output = oracle_output,
+      metrics = "se_point",
+      relative_metrics = "se_point",
+      baseline = "FS-base",
+      by = c("model_id", by)
+    )
+    expected_median_scores <- hubEvals::score_model_out(
+      model_out_tbl = model_out_tbl |> dplyr::filter(.data[["output_type"]] == "median"),
+      oracle_output = oracle_output,
+      metrics = "ae_point",
+      relative_metrics = "ae_point",
+      baseline = "FS-base",
+      by = c("model_id", by)
+    )
+    expected_quantile_scores <- hubEvals::score_model_out(
+      model_out_tbl = model_out_tbl |> dplyr::filter(.data[["output_type"]] == "quantile"),
+      oracle_output = oracle_output,
+      metrics = c("wis", "ae_median", "interval_coverage_50", "interval_coverage_95"),
+      relative_metrics = c("wis", "ae_median"),
+      baseline = "FS-base",
+      by = c("model_id", by)
+    )
+    expected_scores <- expected_mean_scores |>
+      dplyr::left_join(expected_median_scores, by = c("model_id", by)) |>
+      dplyr::left_join(expected_quantile_scores, by = c("model_id", by))
+
+    save_path <- testthat::test_path("testdata", "expected-scores")
+    if (!dir.exists(save_path)) {
+      dir.create(save_path, recursive = TRUE)
+    }
+
+    file_name <- paste0("scores_", window_name, ifelse(is.null(by), "", paste0("_by_", by)), ".csv")
+    write.csv(expected_scores, file = file.path(save_path, file_name), row.names = FALSE)
+  }
+}
+
+make_score_fixtures_one_window("Full season", model_out_tbl)
+make_score_fixtures_one_window("Last 5 weeks", model_out_tbl |> dplyr::filter(reference_date >= "2022-12-17"))
